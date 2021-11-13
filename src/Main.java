9b08aeb4e237ec5bb93c3423f8b76b07d4e11592
gitgud115 <jaredscottdunham@gmail.com>
2021-11-12 22:29:40 -0500

    Testing new code in main.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //test
 public class Main{
 	public static void main(String[] args) {
 		
+		/*
 		Customer cust = new Customer();
 		
 		System.out.println(cust);
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		*/
+		
+		
+		/*
+		List<String> toppings = new ArrayList<String>();
+
+		toppings.add("pineapple");
+		toppings.add("big chungus");
+		
+		Pizza epicPizza = new Pizza("The one to rule them all", 11, 9.0,"Raw Wheat", "The Stuff*", "The Finest Cheese", toppings);
+		Pizza epicPizza2 = new Pizza("The one to rule them all", 11, 9.0,"Raw Wheat", "The Stuff*", "The Finest Cheese", toppings);
+		
+		//System.out.println(epicPizza.toString());
+		
+		Order firstOrder = new Order(cust);
+		
+		firstOrder.addPizzaToOrder(epicPizza);
+		firstOrder.addPizzaToOrder(epicPizza2);
+		
+		System.out.println(firstOrder.printOrderList());
+		*/
 	}
 }
